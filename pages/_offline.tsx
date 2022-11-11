5f12d8401ea3574b4503c752588e45cf62f01aae
John Doe <elliehachemjob@gmail.com>
2022-11-11 16:32:31 +0200

    testing

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,7 +1,11 @@
 import useRouterFunctionalities from '../helpers/router';
+import InnerPage from './inner-page';
 
 export default function Home() {
   const router = useRouterFunctionalities();
 
+  if (router.pathname === '/inner-page') {
+    return <InnerPage />;
+  }
   return <>So here we will render each page</>;
 }
