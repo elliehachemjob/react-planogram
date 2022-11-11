61fc5babd5c9d689d609a4f49c4dc4105de2b05b
John Doe <elliehachemjob@gmail.com>
2022-11-11 13:57:58 +0200

    lazy-loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }: any) {
   return (
     <NoSSR>
-      <Component loading='eager' {...pageProps} />
+      <Component {...pageProps} />
     </NoSSR>
   );
 }
